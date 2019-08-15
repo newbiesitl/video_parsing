24d3f3f948bc07c01d648d30928ee27f26d2f9f7
yzhou <charles.zhou.6006@gmail.com>
2019-08-15 14:39:22 -0700

    add download and open file

--- src/cache.py
+++ src/cache.py
@@ -4,8 +4,10 @@ import requests
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 CACHE_DIR = os.path.join(SCRIPT_PATH, '..', 'cache')
 INDEX_FILE = 'index.txt'
-INDEX_URL = '...'
+INDEX_URL = 'https://hiring.verkada.com/video/index.txt'
+PREFIX = 'https://hiring.verkada.com/video/'
 TS_URL = '...'
+PROJECT_ROOT = SCRIPT_PATH
 
 def get_cache_dir():
     if not os.path.exists(CACHE_DIR):
@@ -17,7 +19,6 @@ def get_cache_dir():
 def get_index_file():
     cache = get_cache_dir()
     filename = os.path.join(cache, INDEX_FILE)
-
     if not os.path.exists(filename):
         download_file(INDEX_URL, filename)
 
@@ -30,7 +31,8 @@ def get_image(timestamp):
 
     timestamp is an integer (seconds since unix epoch)
     '''
-    pass
+    file_url = INDEX_URL % (timestamp)
+    return file_url
 
 
 def download_file(url, filename):
@@ -38,5 +40,42 @@ def download_file(url, filename):
     downloads a the contents of the provided url to a local file
     '''
     contents = requests.get(url).content
-    with open(filename, 'wb') as f:
+    with open(filename, 'wb+') as f:
         f.write(contents)
+
+def video_url(file_name):
+    return PREFIX+file_name
+
+
+if __name__ == "__main__":
+    import cv2
+
+    data_folder = os.path.join(PROJECT_ROOT, 'data')
+    file_list = get_index_file()
+    for this_file in file_list:
+        print(this_file)
+        file_name = this_file.split('/')[-1]
+        file_path = os.path.join(data_folder, file_name)
+        download_file(video_url(this_file), file_path)
+        cap = cv2.VideoCapture(file_path)
+        # Check if camera opened successfully
+        if (cap.isOpened() == False):
+            print("Error opening video stream or file")
+
+        # Read until video is completed
+        while (cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if ret == True:
+
+                # Display the resulting frame
+                cv2.imshow('Frame', frame)
+
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+
+            # Break the loop
+            else:
+                break
+        exit()
