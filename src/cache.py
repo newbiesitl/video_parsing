775820f1f530bfea4322af6b6d034128388fd6be
yzhou <charles.zhou.6006@gmail.com>
2019-08-15 14:49:03 -0700

    crop image

--- src/cache.py
+++ src/cache.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         print(this_file)
         file_name = this_file.split('/')[-1]
         file_path = os.path.join(data_folder, file_name)
-        download_file(video_url(this_file), file_path)
+        # download_file(video_url(this_file), file_path)
         cap = cv2.VideoCapture(file_path)
         # Check if camera opened successfully
         if (cap.isOpened() == False):
@@ -67,10 +67,12 @@ if __name__ == "__main__":
             # Capture frame-by-frame
             ret, frame = cap.read()
             if ret == True:
-
+                x,y = 180,180
+                h, w = 100, 100
+                frame = frame[y:y+h, x:x+w]
+                print(frame.shape)
                 # Display the resulting frame
                 cv2.imshow('Frame', frame)
-
                 # Press Q on keyboard to  exit
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
