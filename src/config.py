7ff72bee4c9a9b6cd148acd28c5a07908815ea10
yzhou <charles.zhou.6006@gmail.com>
2019-08-15 18:50:52 -0700

    crop image

---
+++ src/config.py
@@ -0,0 +1,3 @@
+frame_shape = (100, 100)
+channels = 3
+input_shape = (frame_shape[0], frame_shape[1], channels)
---
+++ src/model_factory/toy_cnn_ae.py
@@ -0,0 +1,34 @@
+
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape
+from keras.models import Model
+from keras import backend as K
+from src.config import input_shape
+
+input_img = Input(shape=input_shape)  # adapt this if using `channels_first` image data format
+
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+flattened = Flatten()(encoded)
+
+print(dir(flattened))
+print(encoded.shape)
+reshaped_2d = Reshape((4, 4, 8))(flattened)
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(reshaped_2d)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+autoencoder.summary()
