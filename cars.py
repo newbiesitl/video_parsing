4d0ee5f61f99a7d865abcf160fb7f43b6a03d89e
yzhou <charles.zhou.6006@gmail.com>
2019-08-25 12:24:55 -0700

    change port to 5050, leave 5000 for docker image

--- cars.py
+++ cars.py
@@ -2,5 +2,5 @@ from restful_service.restful_master_service import *
 
 if __name__ == "__main__":
     # 23450 is http TCP port, need to open this port to external ip
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
     # app.run()
